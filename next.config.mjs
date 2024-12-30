0b48092c79d22ce2e47f74d49a9353c5d93ad75f
faith-nte <faith.nteogwuija@nhs.net>
2024-12-30 23:40:51 +0000

    modified config file

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,9 @@
- /** @type {import('next').NextConfig} */
- const nextConfig = {
+const nextConfig = {
   images: {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: process.env.WORDPRESS_HOSTNAME,
+        hostname: "faithnte.com", // Replace directly for simplicity
         port: "",
         pathname: "/**",
       },
